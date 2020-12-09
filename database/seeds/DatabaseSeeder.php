44ea171d6cb8137ad1818f3df3b73aef563eba0c
gustavo3g <contato.gurustavo@gmail.com>
2020-12-08 22:07:03 -0300

    Migrations Models e configuracoes basicas api

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Database\Eloquent\Model;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Model::unguard();
+
+        $this->call(CompaniesSeed::class);
+        $this->call(JobsSeed::class);
+
+        Model::reguard();
         // $this->call(UserSeeder::class);
     }
 }
